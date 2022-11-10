683d625060b416cdda991914da3715d553e69ae8
Jose Junior <jose@smarppy.com>
2022-11-10 16:05:40 -0300

    add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import GlobalStyles from '../styles/global';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Discovery new places</title>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
