1f9af168019e274a569c8fcd4cb23eb8fe308e3d
Jose Junior <jose@smarppy.com>
2023-02-23 11:12:30 -0300

    Adiciona barra de progresso

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import GlobalStyles from '../styles/global';
-
+import NextNProgress from 'nextjs-progressbar';
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
@@ -10,6 +10,13 @@ const App = ({ Component, pageProps }: AppProps) => {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
     </>
   );
 };
